218c09fdd675b09dd09732cc54400670f6d169dc
enayat789 <enayatbhai2006@gmail.com>
2023-10-03 22:00:57 +0530

    little change

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-// import App from "./App";
+import "./App.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import Header from "./HeaderComponent/Header";
-import Hero from "./heroComponent/Hero";
-import Footer from "./footerComponent/Footer";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <Header />
-    <Hero />
-    <Footer />
-  </>
-);
+root.render(<App />);
 
 reportWebVitals();
